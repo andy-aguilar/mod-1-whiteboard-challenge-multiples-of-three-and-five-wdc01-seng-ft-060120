0af77efe669266d72d7e4ff6726f72c8282c3e5d
andy-aguilar <aaguil3@gmail.com>
2020-06-17 10:02:40 -0400

    done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Enter your procedural solution here!
+a = *(1...1000)
+a_multiples = a.select {|integer| integer % 3 == 0 || integer % 5 == 0}
+puts a_multiples.sum
+
+def collect_multiples(limit)
+    a = *(1...limit)
+    a_multiples = a.select {|integer| integer % 3 == 0 || integer % 5 == 0}
+    return a_multiples
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+
+    def initialize (limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        a = *(1...limit)
+        a_multiples = a.select {|integer| integer % 3 == 0 || integer % 5 == 0} 
+        return a_multiples
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
